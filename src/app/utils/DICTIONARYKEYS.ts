8e00bcfc5c0432543fe6cd6b2f6cdf87d409c551
ibonillavillarreal <ibonillavillarreal@gmail.com>
2023-08-01 15:14:43 -0600

    1er Despliege

--- src/app/utils/DICTIONARYKEYS.ts
+++ src/app/utils/DICTIONARYKEYS.ts
@@ -1,9 +1,9 @@
 
 export  class DICTIONARYKEYS{
-        public  url: string = "http://20.163.27.210/"; 
+        public  url: string = "http://localhost:3000"; 
         //public  url: string = "http://192.168.250.232:3000"; 
         //public  url: string = "http://172.16.23.26:3000"; 
-        public  urlprint: string = "http://20.163.27.210/"; 
+        public  urlprint: string = "http://localhost:3000"; 
         //public  urlprint: string = "http://192.168.250.232:4900"; 
         // public  urlprint: string = "http://172.16.26.18:4900"; 
         public static  IDTIPO  = 213;
